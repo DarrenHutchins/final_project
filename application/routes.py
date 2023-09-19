f1833efab7e42356acf4c86664b35593664f2911
Darren Hutchins <darrenhutchins111@gmail.com>
2023-09-19 20:51:41 +0100

    chnages with flask

--- application/routes.py
+++ application/routes.py
@@ -8,3 +8,11 @@ def connect_db():
         user = 'root', password = 'password', database = 'sakila',
         autocommit = True, charset = 'utf8mb4',
         cursorclass = pymysql.cursors.DictCursor)
+
+@app.route('/')
+def home():
+    """Landing page. Showing Actors    """
+
+    return render_template(
+                'home.html'
+    )
